eac4a419c4052460da0305131368d0d4b1ff8d9d
Dileep Kishore <k.dileep1994@gmail.com>
2018-02-12 10:25:54 -0500

    Added some animations to the header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Grid, Image, Segment, Button } from 'semantic-ui-react';
+import { Grid, Image, Segment, Button, Icon, } from 'semantic-ui-react';
 import MenuComponent, { MenuItem } from './Menu';
 const microbiomeBanner = require('../images/banner_microbiome.png');
 import { css } from 'react-emotion';
@@ -23,7 +23,10 @@ function goToMicrobiomePage(): void {
 
 function HeaderComponent(): JSX.Element {
   return (
-    <Segment style={{background: `linear-gradient(to right, #434343, #000000)`}} className={bgStyle}>
+    <Segment
+      style={{background: `linear-gradient(to right, #434343, #000000)`}}
+      className={bgStyle}
+    >
       <Grid container={true}>
         <Grid.Row>
           <Grid.Column>
@@ -48,8 +51,14 @@ function HeaderComponent(): JSX.Element {
               color="pink"
               size="large"
               onClick={goToMicrobiomePage}
+              animated={true}
             >
-              Microbiome Day 2018
+              <Button.Content visible={true}>
+                <Icon name="bug"/> Microbiome Day 2018
+              </Button.Content>
+              <Button.Content hidden={true}>
+                Feb 14, 2018 <Icon name="arrow right"/>
+              </Button.Content>
             </Button>
           </Grid.Column>
         </Grid.Row>
