ca7e7d4e6d1ebbdb05b7932cabb8d094672c11ad
Dileep Kishore <k.dileep1994@gmail.com>
2018-02-15 16:52:07 -0500

    Updated the tool list

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -3,12 +3,14 @@ import { Grid } from 'semantic-ui-react';
 import PathoStatComponent from './Pathostat';
 import PathoScopeComponent from './Pathoscope';
 import CometsComponent from './Comets';
+import MindComponent from './Mind';
 
 function ToolsComponent(): JSX.Element {
   return (
     <Grid container={true}>
       <Grid.Row>
         <CometsComponent />
+        <MindComponent />
         <PathoStatComponent />
         <PathoScopeComponent />
       </Grid.Row>
